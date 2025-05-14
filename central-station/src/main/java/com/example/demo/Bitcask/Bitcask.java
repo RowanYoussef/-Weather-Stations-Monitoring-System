321cca99cb16cab1da87a13fe873f0b0c91f33fe
porvah <3msabdullah@gmail.com>
2025-05-14 23:40:42 +0300

    adding recovery feature

--- central-station/src/main/java/com/example/demo/Bitcask/Bitcask.java
+++ central-station/src/main/java/com/example/demo/Bitcask/Bitcask.java
@@ -1,14 +1,37 @@
 package com.example.demo.Bitcask;
 
+import java.io.*;
+import java.nio.charset.StandardCharsets;
 import java.util.HashMap;
 
 public class Bitcask implements  BitcaskI{
-    HashMap<Long , byte[]>  map;
-    String currentFileNo;
+    private final String baseDataDir = "bin/";
+    private final String baseHintDir = "hint/";
+    private HashMap<Long , CaskItem>  map = new HashMap<>();;
+    private String currentFileNo;
+    private RandomAccessFile currentFile;
+    private long currentOffset;
 
-    public Bitcask(){
-        map = new HashMap<>();
+    public Bitcask () throws IOException{
+        recoverFromHintFiles();
     }
+
+    private void recoverFromHintFiles() throws IOException {
+        File hintDir = new File(baseHintDir);
+        File[] hints = hintDir.listFiles((file, name) -> name.endsWith(".hint"));
+        if (hints == null) return;
+        for (File hint: hints){
+            DataInputStream input = new DataInputStream(new FileInputStream(hint));
+            while (input.available() > 0){
+                long key = input.readLong();
+                int valLength = input.readInt();
+                long offset = input.readLong();
+                String fileName = hint.getName().replace(".hint", ".data");
+                map.put(key, new CaskItem(fileName, offset, valLength));
+            }
+        }
+    }
+
     @Override
     public void put(long key, byte[] value) {
 
@@ -19,7 +42,5 @@ public class Bitcask implements  BitcaskI{
         return new byte[0];
     }
 
-    private void recover(){
-        
-    }
+
 }

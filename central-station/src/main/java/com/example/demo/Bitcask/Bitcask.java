9384f9c8eb6f20a603e6e4fe4169241b719b8a07
Rowan Youssef <rowanmohammad667@gmail.com>
2025-05-14 22:31:23 +0300

    yarab el ra7ma 34an t3ebt

--- central-station/src/main/java/com/example/demo/Bitcask/Bitcask.java
+++ central-station/src/main/java/com/example/demo/Bitcask/Bitcask.java
@@ -1,4 +1,25 @@
 package com.example.demo.Bitcask;
 
-public class Bitcask {
+import java.util.HashMap;
+
+public class Bitcask implements  BitcaskI{
+    HashMap<Long , byte[]>  map;
+    String currentFileNo;
+
+    public Bitcask(){
+        map = new HashMap<>();
+    }
+    @Override
+    public void put(long key, byte[] value) {
+
+    }
+
+    @Override
+    public byte[] get(long key) {
+        return new byte[0];
+    }
+
+    private void recover(){
+        
+    }
 }
--- central-station/src/main/java/com/example/demo/Bitcask/BitcaskI.java
+++ central-station/src/main/java/com/example/demo/Bitcask/BitcaskI.java
@@ -1,4 +1,6 @@
 package com.example.demo.Bitcask;
 
 public interface BitcaskI {
+    public void put(long key , byte[] value);
+    public byte[] get(long key);
 }

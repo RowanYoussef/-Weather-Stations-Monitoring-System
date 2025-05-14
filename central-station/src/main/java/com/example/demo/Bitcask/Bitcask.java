515dd67b483afe7f47b4d6f78f9375dff97eaa63
porvah <3msabdullah@gmail.com>
2025-05-14 23:56:52 +0300

    log partitioning implementation

--- central-station/src/main/java/com/example/demo/Bitcask/Bitcask.java
+++ central-station/src/main/java/com/example/demo/Bitcask/Bitcask.java
@@ -3,6 +3,9 @@ package com.example.demo.Bitcask;
 import java.io.*;
 import java.nio.charset.StandardCharsets;
 import java.util.HashMap;
+import java.util.concurrent.Executor;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
 
 public class Bitcask implements  BitcaskI{
     private final String baseDataDir = "bin/";
@@ -11,9 +14,32 @@ public class Bitcask implements  BitcaskI{
     private String currentFileNo;
     private RandomAccessFile currentFile;
     private long currentOffset;
-
+    private final int maxFileSize = 1024;
+    private int fileCounter = 0;
     public Bitcask () throws IOException{
         recoverFromHintFiles();
+        partitionLogs();
+        cleanupSchedule();
+    }
+
+    private void cleanupSchedule() {
+        Executors.newSingleThreadScheduledExecutor().scheduleAtFixedRate(()->{
+            try{
+                compactFiles();
+            }catch(Exception ignored){}
+        }, 1, 5, TimeUnit.MINUTES);
+    }
+
+    private void compactFiles() {
+    }
+
+    private void partitionLogs() throws IOException {
+        if (currentFile == null || currentFile.length() > maxFileSize){
+            if (currentFile != null) currentFile.close();
+            currentFileNo = String.format("%08d.data", ++fileCounter);
+            currentFile = new RandomAccessFile(baseDataDir+currentFileNo, "rw");
+            currentOffset = 0;
+        }
     }
 
     private void recoverFromHintFiles() throws IOException {
